6508565a7848d9f4e57dc0850ac228f64575493b
Ramin Safarov <silent1962@gmail.com>
2025-05-15 00:23:30 +0300

    example: правки

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -1,6 +1,8 @@
 
 import pytest
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
